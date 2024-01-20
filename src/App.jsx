8be72ac686fe82e903dc51188dacfcda9673f76d
Manuel Guest <manuelguest@Manuels-MacBook-Pro-2.local>
2024-01-21 00:35:34 +0100

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import "./App.css";
 import { MainList } from "./components";
+// import { Amplify, AuthModeStrategyType } from "aws-amplify";
+// import awsExports from "./aws-exports";
+//AWS
+// Amplify.configure({
+//   ...awsExports,
+//   aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
+//   DataStore: {
+//     authModeStrategyType: AuthModeStrategyType.MULTI_AUTH,
+//   },
+// });
 
 const App = () => {
   return (
