618058c5aef23a31ffc3d5e562e6222a3155dbc5
Manuel Guest <manuelguest@Manuels-MacBook-Pro-2.local>
2024-01-27 01:08:09 +0100

    removed superfluous tags

--- src/constants/TAGS.js
+++ src/constants/TAGS.js
@@ -1,23 +1,12 @@
 const ADJECTIVE = 'adjective';
 const CLOTHES = 'clothes';
 const COUNTRY = 'country';
-const COLOR = 'color';
-const DRINK = 'drink';
 const ENTERTAINMENT = 'entertainment';
 const FAMILY = 'family';
 const FOOD = 'food';
-const FOREIGN = 'foreign';
 const FORMALITY = 'formality';
-const GREETING = 'greeting';
-const HIRAGANA = 'hiragana';
-const HOUSE = 'house';
 const KANJI = 'kanji';
 const KATAKANA = 'katakana';
-const LITERATURE = 'literature';
-const LOCATION = 'location';
-const MEDICINE = 'medicine';
-const MUSIC = 'music';
-const NATIONALITY = 'nationality';
 const NOUN = 'noun';
 const NUMBER = 'number';
 const OCCUPATION = 'occupation';
@@ -25,38 +14,24 @@ const OTHER = 'other';
 const PARTICLE = 'particle';
 const PLACE = 'place';
 const PERSON = 'person';
-const RAIN = 'rain';
 const SCHOOL = 'school';
-const SIZE = 'size';
 const SPORT = 'sport';
 const TECH = 'tech';
 const THING = 'thing';
 const TIME = 'time';
 const TRANSPORT = 'transport';
-const VACATION = 'vacation';
 const VERB = 'verb';
 
 const TAGS = [
   ADJECTIVE,
   CLOTHES,
   COUNTRY,
-  COLOR,
-  DRINK,
   ENTERTAINMENT,
   FAMILY,
   FOOD,
-  FOREIGN,
   FORMALITY,
-  GREETING,
-  HIRAGANA,
-  HOUSE,
   KANJI,
   KATAKANA,
-  LITERATURE,
-  LOCATION,
-  MEDICINE,
-  MUSIC,
-  NATIONALITY,
   NOUN,
   NUMBER,
   OCCUPATION,
@@ -64,15 +39,12 @@ const TAGS = [
   PARTICLE,
   PERSON,
   PLACE,
-  RAIN,
   SCHOOL,
-  SIZE,
   SPORT,
   TECH,
   THING,
   TIME,
   TRANSPORT,
-  VACATION,
   VERB,
 ];
 
@@ -80,30 +52,18 @@ export default TAGS;
 export {
   CLOTHES,
   COUNTRY,
-  COLOR,
-  DRINK,
   ENTERTAINMENT,
   FAMILY,
   FOOD,
   FORMALITY,
-  GREETING,
-  HOUSE,
-  LITERATURE,
-  LOCATION,
-  MEDICINE,
-  MUSIC,
-  NATIONALITY,
   NUMBER,
   OCCUPATION,
   PERSON,
   PLACE,
-  RAIN,
   SCHOOL,
-  SIZE,
   SPORT,
   TECH,
   THING,
   TIME,
   TRANSPORT,
-  VACATION,
 };
