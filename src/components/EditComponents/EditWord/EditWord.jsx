5d64fe6f05a7d0b788f777e59a259902111aa232
Manuel Guest <manuelguest@Manuels-MacBook-Pro-2.local>
2024-09-24 01:00:34 +0200

    more style

--- src/components/EditComponents/EditWord/EditWord.jsx
+++ src/components/EditComponents/EditWord/EditWord.jsx
@@ -22,14 +22,17 @@ function EditWord(props) {
 
   return (
     <div className="edit-word">
-      <div className="edit-word-display">
-        <span>{word}</span>
+      <div className="edit-word__display">
+        <div className="edit-word__display__word">
+          {word.split('').map((char) => <span className="edit-word__display__word__char">{char}</span>)}
+        </div>
+
         {itemAlreadyExists && (
-          <span className="edit-word-error-message">Word already exists</span>
+          <span className="edit-word__display__error-message">Word already exists</span>
         )}
       </div>
-      <div className="edit-word-actions">
-        <div className="add-furi-button">
+      <div className="edit-word__actions">
+        <div className="edit-word__actions__furi-button">
           <Button
             isDisabled={isFuriButtonDisabled}
             onClick={handleGoToFuriEditStep}
@@ -38,7 +41,7 @@ function EditWord(props) {
           </Button>
         </div>
         <form
-          className="edit-word-input"
+          className="edit-word__actions__input"
           onSubmit={handleOnSubmit}
         >
           <Input
@@ -47,7 +50,7 @@ function EditWord(props) {
             onChange={handleOnChange}
             onKeyDown={handleKeyDown}
           />
-          <div className="edit-word-input-buttons">
+          <div className="edit-word__actions__input__buttons">
             <Button
               isNotSelected={!isHiraganaSelected}
               onClick={() => handleKanaClick('hi')}
